992754577147c36eecd587f51ea7a3b6866fd984
palanisamy90100 <palanisamy.madakkannu@gmail.com>
2024-12-21 16:01:41 +0530

    First Commit

---
+++ src/main/java/com/codesnippetst/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.codesnippetst.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/codesnippetst/springcoredemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.codesnippetst.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Batting/Bowling for 30min!!";
+    }
+}
